df6f1570b20844d0f3f5c46fe7b13fe13398c084
luiz.marcelo <luiz.marcelo@idwall.co>
2023-01-10 00:32:24 -0300

    Adicionando metodos

--- PythoEntendendoOrientacaoObjetos/conta.py
+++ PythoEntendendoOrientacaoObjetos/conta.py
@@ -7,14 +7,22 @@ class Conta:
         self.__saldo = saldo
         self.__limite = limite
 
+
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.__saldo,self.__titular))
 
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = (self.__saldo + self.__limite)
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
@@ -33,4 +41,10 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+    @staticmethod
+    def codigo_banco():
+        return "001"
 
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
